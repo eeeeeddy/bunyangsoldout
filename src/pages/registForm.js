74693e225576fc25b66d9c3662ba03aaecef97cb
eeeeeddy <marill94727@naver.com>
2024-11-24 17:26:37 +0900

    netlify form 작업(7)

--- src/pages/registForm.js
+++ src/pages/registForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { PC, Mobile } from "../components/responsive";
 import Navbar from './navBar';
 import ActionButton from './actionButton';
