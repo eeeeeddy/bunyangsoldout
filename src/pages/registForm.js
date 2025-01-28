d80fa0b1ef84fc52b30875a91a21a9fecc55bc3b
eeeeeddy <marill94727@naver.com>
2025-01-29 01:23:56 +0900

    관심고객등록 페이지 Navbar 컴포넌트 주석 처리 (별도 메뉴바 사용)

--- src/pages/registForm.js
+++ src/pages/registForm.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { PC, Mobile } from "../components/responsive";
-import Navbar from './navBar';
+// import Navbar from './navBar';
 import ActionButton from './actionButton';
 import emailjs from '@emailjs/browser';
 
